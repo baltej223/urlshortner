bb9b70f4f65277d99992e08fc9f632c235cf60b0
Baltej Singh <baltej963@gmail.com>
2025-03-25 19:42:18 +0530

    Update page.jsx

--- app/[key]/page.jsx
+++ app/[key]/page.jsx
@@ -9,13 +9,6 @@ export default async function Page({ params }) {
 
   console.log(key);
   
-  // fetch("./"+key);
-
-  let document = await model.findOne({key:key}).exec();
-  console.log("document found will key at dynamic url:", document);
-  if (document){
-    let originalUrl = document.url;
-    redirect(originalUrl);
-  }
-  redirect("./"); 
+  let res = await fetch("/api/getUrl/"+key);
+  redirect(res.url?res.url:"./");
 }
